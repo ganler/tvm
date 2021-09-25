2e3ca1526ec46bf0b2c80f5f1200efa7833a41c0
ganler <jaway.liu@gmail.com>
2021-09-24 23:25:39 -0500

    fix: able to deserialize JSON files

--- fuzz_me.cc
+++ fuzz_me.cc
@@ -1,11 +1,16 @@
 #include <tvm/driver/driver_api.h>
 #include <tvm/parser/parser.h>
 #include <tvm/target/target.h>
+#include <tvm/node/serialization.h>
+#include <tvm/node/node.h>
+#include "tvm/ir/module.h"
+#include <tvm/runtime/object.h>
 
 #include <cstdint>
 #include <ctime>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <string_view>
 
 /*
@@ -36,12 +41,13 @@ ninja
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   try {
     std::string data(Size, '0');
-  for (size_t i=0; i < Size; ++i)
-    data[i] = Data[i];
-  auto ir_module = tvm::parser::ParseModule("GeneratedSource", data);
-  auto module = tvm::build(ir_module, tvm::Target("llvm"), tvm::Target("llvm"));
+    for (size_t i=0; i < Size; ++i)
+      data[i] = Data[i];
+
+    tvm::IRModule ir_mod(tvm::Downcast<tvm::IRModule>(tvm::LoadJSON(data)));
+    auto module = tvm::build(ir_mod, tvm::Target("llvm"), tvm::Target("llvm"));
   } catch (...) {
-    std::cout << "----- [LOG][Tzer.Baseline.LibFuzzer] Some error occurred... ";
+    std::cout << "----- [LOG][Tzer.Baseline.LibFuzzer] Some error occurred... " << std::endl;
   }
   return 0;  // Non-zero return values are reserved for future use.
 }
